e4f78162905330542e24bbccf4c986f3f74a23c8
Adam Frisch <afrisch88@outlook.com>
2022-02-18 16:11:38 -0600

    attempting to polish up code

--- super_heroes/serializers.py
+++ super_heroes/serializers.py
@@ -7,14 +7,3 @@ class SuperHeroSerializer(serializers.ModelSerializer):
         fields =['id','alter_ego','name','primary_ability','secondary_ability','catch_phrase', 'super_type','super_type_id']
 
     super_type_id = serializers.IntegerField(write_only = True)
-
-
-
-
-
-# class SuperTypeserializers(serializers.ModelSerializer):
-#     class Meta :
-#         model = SuperType
-#         fields = ['id','type']
-#         depth = 1
-
--- supers_types/urls.py
+++ supers_types/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns =[
     path('',views.super_types_list),
-    #path('<int:pk>/', views.super_type_detail),  # for setup of delete or edit but not logic in views currently
+    path('<int:pk>/', views.superheroes_detail), 
 ]
+   
